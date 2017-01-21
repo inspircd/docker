75da69618b0ead64dc57cca31754021bfaec1f57
Sheogorath <sheogorath@shivering-isles.com>
2017-01-21 07:26:22 +0100

    Improve test file

--- tests/mountConfig.sh
+++ tests/mountConfig.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "
+         ######################################
+         ###      Mounting config test       ##
+         ######################################
+"
+
+
 # Make sure tests fails if a commend ends without 0
 set -e
 
@@ -19,19 +26,20 @@ TLS_SERVER_PORT=$(cat /dev/urandom|od -N2 -An -i|awk -v f=40000 -v r=49999 '{pri
 
 
 # Create config directory for testing
-mkdir testconf && sudo chown 10000 testconf
+mkdir /tmp/test-mountConfig/ && sudo chown 10000 /tmp/test-mountConfig/
 
 # Create docker container with our test parameters
-DOCKERCONTAINER=$(docker run -d -v $(pwd)/testconf:/inspircd/conf -p 127.0.0.1:${CLIENT_PORT}:6667 -p 127.0.0.1:${TLS_CLIENT_PORT}:6697 inspircd:testing)
+DOCKERCONTAINER=$(docker run -d -v /tmp/test-mountConfig/:/inspircd/conf -p 127.0.0.1:${CLIENT_PORT}:6667 -p 127.0.0.1:${TLS_CLIENT_PORT}:6697 inspircd:testing)
 sleep 5
 
 # Check if config was created successfully
-diff conf/inspircd.conf testconf/inspircd.conf
+diff conf/inspircd.conf /tmp/test-mountConfig/inspircd.conf
 echo quit | timeout 10 openssl s_client -ign_eof -connect localhost:${TLS_CLIENT_PORT}
 
 # Make sure the internal healthcheck is working
 sleep 28
 [ $(docker ps -f id=${DOCKERCONTAINER} | grep \(healthy\) | wc -l) -eq 1 ] || exit 1
 
+# Clean up
 docker stop ${DOCKERCONTAINER} && docker rm ${DOCKERCONTAINER}
 
