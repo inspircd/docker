366efbe6535ca837cc82a242dac887fdaf6ab9c5
Sheogorath <sheogorath@shivering-isles.com>
2017-04-29 18:55:26 +0200

    Centralize test environment code

--- tests/customBuild.sh
+++ tests/customBuild.sh
@@ -9,18 +9,7 @@ echo "
 # Make sure tests fails if a commend ends without 0
 set -e
 
-# Generate some random ports for testing
-CLIENT_PORT=$(cat /dev/urandom|od -N2 -An -i|awk -v f=10000 -v r=19999 '{printf "%i\n", f + r * $1 / 65536}')
-TLS_CLIENT_PORT=$(cat /dev/urandom|od -N2 -An -i|awk -v f=20000 -v r=29999 '{printf "%i\n", f + r * $1 / 65536}')
-SERVER_PORT=$(cat /dev/urandom|od -N2 -An -i|awk -v f=30000 -v r=39999 '{printf "%i\n", f + r * $1 / 65536}')
-TLS_SERVER_PORT=$(cat /dev/urandom|od -N2 -An -i|awk -v f=40000 -v r=49999 '{printf "%i\n", f + r * $1 / 65536}')
-
-
-# Make sure the ports are not already in use. In case they are rerun the script to get new ports.
-[ $(netstat -an | grep LISTEN | grep :$CLIENT_PORT | wc -l) -eq 0 ] || { ./$0 && exit 0 || exit 1; }
-[ $(netstat -an | grep LISTEN | grep :$TLS_CLIENT_PORT | wc -l) -eq 0 ] || { ./$0 && exit 0 || exit 1; }
-[ $(netstat -an | grep LISTEN | grep :$SERVER_PORT | wc -l) -eq 0 ] || { ./$0 && exit 0 || exit 1; }
-[ $(netstat -an | grep LISTEN | grep :$TLS_SERVER_PORT | wc -l) -eq 0 ] || { ./$0 && exit 0 || exit 1; }
+. $(dirname "$0")/.portconfig.sh
 
 # Create config directory for testing
 mkdir /tmp/test-customBuild/
