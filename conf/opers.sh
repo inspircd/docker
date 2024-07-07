6fd16456259c361b0336c0c477715397b3c4aea6
Sadie Powell <sadie@witchery.services>
2024-07-07 21:14:35 +0100

    Fix some switched fields.

--- conf/opers.sh
+++ conf/opers.sh
@@ -20,8 +20,8 @@ cat <<EOF
 <define name="operName" value="${INSP_OPER_NAME:-oper}">
 <define name="operPassword" value="${INSP_OPER_PASSWORD_HASH}">
 <define name="operFingerprint" value="${INSP_OPER_FINGERPRINT}">
-<define name="operAutologin" value="${INSP_OPER_SSLONLY:-yes}">
-<define name="operSSLOnly" value="${INSP_OPER_AUTOLOGIN:-yes}">
+<define name="operAutologin" value="${INSP_OPER_AUTOLOGIN:-yes}">
+<define name="operSSLOnly" value="${INSP_OPER_SSLONLY:-yes}">
 <define name="operHash" value="${INSP_OPER_HASH:-hmac-sha256}">
 <define name="operHost" value="${INSP_OPER_HOST:-*@*}">
 EOF
